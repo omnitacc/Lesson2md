1aaeb0344f098deb9921bec5a0d6179fa8866542
Mihails Smirnovs <omnitacc@gmail.com>
2021-09-13 16:41:48 +0300

    adding project

---
+++ Lesson2md/Lesson2md/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson2md
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kā tevi sauc?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Sveiks {userName}");
+            Console.WriteLine("----------------------------------------------?");
+
+            Console.WriteLine("Kāds ir tavs vecums?");
+            string userAgeText = Console.ReadLine();
+            int userAge = int.Parse(userAgeText);
+            int userAgeNext = userAge + 1;
+            bool isAdult = userAge >= 18;
+            Console.WriteLine($"Nākamgad tev paliks {userAgeNext}, Tu esi pilngadīgs: {isAdult}");
+
+
+        }
+    }
+}
